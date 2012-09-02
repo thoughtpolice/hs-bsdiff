ffc2de0b3720f237cf2a2b741f30fceb05ee2702
Austin Seipp <mad.one@gmail.com>
2012-09-02 05:05:46 -0500

    Example
    Signed-off-by: Austin Seipp <mad.one@gmail.com>

--- examples/ex1.hs
+++ examples/ex1.hs
@@ -2,7 +2,11 @@ module Main
        ( main -- :: IO ()
        ) where
 
+import System.Environment (getArgs)
 import Data.ByteString.BSDiff
 
 main :: IO ()
-main = return ()
+main = getArgs >>= go
+  where go ["diff",inf,out,patch]  = bsdiff inf out patch
+        go ["patch",inf,patch,out] = bspatch inf patch out
+        go _                         = return ()
