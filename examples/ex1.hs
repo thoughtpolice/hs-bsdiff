d466f4b151ae01ac57c58fae02d17f668e871f48
Austin Seipp <mad.one@gmail.com>
2012-09-02 18:16:16 -0500

    Refactor example to be like minibsdiff, and show off compression
    Signed-off-by: Austin Seipp <mad.one@gmail.com>

--- examples/ex1.hs
+++ examples/ex1.hs
@@ -2,11 +2,24 @@ module Main
        ( main -- :: IO ()
        ) where
 
+import Codec.Compression.LZ4 (compressPlusHC, decompressPlusHC)
+
 import System.Environment (getArgs)
 import Data.ByteString.BSDiff
 
 main :: IO ()
 main = getArgs >>= go
-  where go ["diff",inf,out,patch]  = bsdiff inf out patch
-        go ["patch",inf,patch,out] = bspatch inf patch out
-        go _                         = return ()
+  where go ["-h"]             = usage
+        go ["gen",i,o,p]      = bsdiff i o p
+        go ["app",o,p,n]      = bspatch o p n
+        go ["-c","gen",i,o,p] = bsdiff' compressPlusHC i o p
+        go ["-c","app",o,p,n] = bspatch' decompressPlusHC o p n
+        go _                  = usage
+
+usage :: IO ()
+usage = putStrLn $ "usage:\n\n" ++
+                   "Generate patch:\n" ++
+                   "\t$ ex1 [-c] gen <v1> <v2> <patch>\n" ++
+                   "Apply patch:\n" ++
+                   "\t$ ex1 [-c] app <v1> <patch> <v2>\n\n" ++
+                   "The '-c' option enables lz4 compression"
