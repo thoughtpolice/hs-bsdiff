8eb9adc54c1cfdbd8b12c33d0987f9cee0631729
Austin Seipp <mad.one@gmail.com>
2012-09-02 21:46:20 -0500

    Now with tests!
    Signed-off-by: Austin Seipp <mad.one@gmail.com>

--- tests/Properties.hs
+++ tests/Properties.hs
@@ -1,19 +1,30 @@
+{-# OPTIONS_GHC -fno-cse #-}
 module Main
        ( main -- :: IO ()
        ) where
 
 import Test.Hspec
 import Test.QuickCheck
+import Test.QuickCheck.Instances()
 
-import Data.ByteString
+import qualified Data.ByteString as S(null)
 import Data.ByteString.BSDiff
 
 main :: IO ()
 main = hspec $ do
   -------------------------------------------------------------------------------
   -- Diffing
-  describe "diffing" $ return ()
+  describe "diffing" $
+    it "is a pure operation" $ property $
+     \old new -> diff old new == diff old new
 
   -------------------------------------------------------------------------------
   -- Patching
-  describe "patching" $ return ()
+  describe "patching" $ do
+    it "is a pure operation" $ property $
+      \old new -> (patch old =<< diff old new) == (patch old =<< diff old new)
+
+    it "returns a new version, given a patch and an old version" $ property $
+      \old new -> not (S.null old || S.null new) ==>
+                   Just new == (patch old =<< diff old new)
+
